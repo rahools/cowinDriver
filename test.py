ab7b71ebc31f6427acfaf5a0a1beb241d12d9201
Rahul Singh <contact@rahools.com>
2021-05-17 15:01:11 +0530

    blank login workaround added.

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ import time
 # %%
 if __name__ == '__main__':
     driver = CoWinDriver()
+    webdriver = driver.getDriver()
 
     mobileNumber = ''   # mobile Number
     ip = ''             # Phone/airmore ip addr
@@ -17,6 +18,17 @@ if __name__ == '__main__':
     time.sleep(10)
     driver.inputOTP(ip)
     time.sleep(10)
+
+    # idk why login returns a blank page. might have to look into it.
+    # for now here's a shoddy work around.
+    webdriver.refresh()
+    time.sleep(5)
+
+    driver.inputMobile(mobileNumber)
+    time.sleep(10)
+    driver.inputOTP(ip)
+    time.sleep(10)
+
     driver.autoBookingNav(state, district)
 
 # %%
